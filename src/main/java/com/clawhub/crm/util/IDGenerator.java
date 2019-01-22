110e2688247f5609e96a110d21864163cd1687df
李志明 <18120177709@163.com>
2019-01-22 21:58:29 +0800

    完善代码

--- src/main/java/com/clawhub/crm/util/IDGenerator.java
+++ src/main/java/com/clawhub/crm/util/IDGenerator.java
@@ -1,5 +1,7 @@
 package com.clawhub.crm.util;
 
+import java.util.UUID;
+
 /**
  * <Description> ID生成器<br>
  *
@@ -9,4 +11,21 @@ package com.clawhub.crm.util;
  * @create 2019-01-21 21:35<br>
  */
 public class IDGenerator {
+    /**
+     * Get id string.
+     *
+     * @return the string
+     */
+    public static String getID() {
+        return UUID.randomUUID().toString().replaceAll("-", "");
+    }
+
+    /**
+     * Gets employee id.
+     *
+     * @return the employee id
+     */
+    public static String getEmployeeID() {
+        return "日期" + "标号";
+    }
 }
