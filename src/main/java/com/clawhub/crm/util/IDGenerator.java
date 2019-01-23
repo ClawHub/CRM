27670970ba4fd7424a54998ca4339602179886cc
John Doe <736365465@qq.com>
2019-01-23 21:14:31 +0800

    资源管理 部门管理 用户管理

--- src/main/java/com/clawhub/crm/util/IDGenerator.java
+++ src/main/java/com/clawhub/crm/util/IDGenerator.java
@@ -1,5 +1,7 @@
 package com.clawhub.crm.util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -26,6 +28,14 @@ public class IDGenerator {
      * @return the employee id
      */
     public static String getEmployeeID() {
-        return "日期" + "标号";
+        Date date = new Date();
+        // 日期：年月日时分秒
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("YYYYMMDDhhmmss");
+        String time = simpleDateFormat.format(date);
+        //生成一个1000到 2000的随机数
+        int i = (int)(Math.random()*1000+1000);
+        // 拼接成id
+        String employeeID = time + String.valueOf(i);
+        return employeeID;
     }
 }
