2bd77469bfb96a81d2b36d7ed2a7dc553b90711b
John Doe <736365465@qq.com>
2019-01-24 21:03:26 +0800

    角色管理 角色资源关系 用户角色关系

---
+++ src/main/java/com/clawhub/crm/entity/Employee.java
@@ -0,0 +1,292 @@
+package com.clawhub.crm.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Table(name = "t_employee")
+public class Employee {
+    /**
+     * id
+     */
+    private String id;
+
+    /**
+     * 创建时间
+     */
+    @Column(name = "create_time")
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    @Column(name = "update_time")
+    private Date updateTime;
+
+    /**
+     * 创建人
+     */
+    @Column(name = "create_user")
+    private String createUser;
+
+    /**
+     * 更新人
+     */
+    @Column(name = "update_user")
+    private String updateUser;
+
+    /**
+     * 备注
+     */
+    private String remark;
+
+    /**
+     * 删除状态 0:未删除 1:已删除
+     */
+    private String deletestate;
+
+    /**
+     * 员工id
+     */
+    @Column(name = "employee_id")
+    private String employeeId;
+
+    /**
+     * 员工姓名
+     */
+    @Column(name = "employee_name")
+    private String employeeName;
+
+    /**
+     * 性别0:male 1:female
+     */
+    private String sex;
+
+    /**
+     * 员工职称
+     */
+    @Column(name = "employee_title")
+    private String employeeTitle;
+
+    /**
+     * 用户id
+     */
+    @Column(name = "user_id")
+    private String userId;
+
+    /**
+     * 获取id
+     *
+     * @return id - id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * 设置id
+     *
+     * @param id id
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * 获取创建时间
+     *
+     * @return create_time - 创建时间
+     */
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    /**
+     * 设置创建时间
+     *
+     * @param createTime 创建时间
+     */
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    /**
+     * 获取更新时间
+     *
+     * @return update_time - 更新时间
+     */
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    /**
+     * 设置更新时间
+     *
+     * @param updateTime 更新时间
+     */
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    /**
+     * 获取创建人
+     *
+     * @return create_user - 创建人
+     */
+    public String getCreateUser() {
+        return createUser;
+    }
+
+    /**
+     * 设置创建人
+     *
+     * @param createUser 创建人
+     */
+    public void setCreateUser(String createUser) {
+        this.createUser = createUser;
+    }
+
+    /**
+     * 获取更新人
+     *
+     * @return update_user - 更新人
+     */
+    public String getUpdateUser() {
+        return updateUser;
+    }
+
+    /**
+     * 设置更新人
+     *
+     * @param updateUser 更新人
+     */
+    public void setUpdateUser(String updateUser) {
+        this.updateUser = updateUser;
+    }
+
+    /**
+     * 获取备注
+     *
+     * @return remark - 备注
+     */
+    public String getRemark() {
+        return remark;
+    }
+
+    /**
+     * 设置备注
+     *
+     * @param remark 备注
+     */
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    /**
+     * 获取删除状态 0:未删除 1:已删除
+     *
+     * @return deletestate - 删除状态 0:未删除 1:已删除
+     */
+    public String getDeletestate() {
+        return deletestate;
+    }
+
+    /**
+     * 设置删除状态 0:未删除 1:已删除
+     *
+     * @param deletestate 删除状态 0:未删除 1:已删除
+     */
+    public void setDeletestate(String deletestate) {
+        this.deletestate = deletestate;
+    }
+
+    /**
+     * 获取员工id
+     *
+     * @return employee_id - 员工id
+     */
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    /**
+     * 设置员工id
+     *
+     * @param employeeId 员工id
+     */
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    /**
+     * 获取员工姓名
+     *
+     * @return employee_name - 员工姓名
+     */
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    /**
+     * 设置员工姓名
+     *
+     * @param employeeName 员工姓名
+     */
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    /**
+     * 获取性别0:male 1:female
+     *
+     * @return sex - 性别0:male 1:female
+     */
+    public String getSex() {
+        return sex;
+    }
+
+    /**
+     * 设置性别0:male 1:female
+     *
+     * @param sex 性别0:male 1:female
+     */
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    /**
+     * 获取员工职称
+     *
+     * @return employee_title - 员工职称
+     */
+    public String getEmployeeTitle() {
+        return employeeTitle;
+    }
+
+    /**
+     * 设置员工职称
+     *
+     * @param employeeTitle 员工职称
+     */
+    public void setEmployeeTitle(String employeeTitle) {
+        this.employeeTitle = employeeTitle;
+    }
+
+    /**
+     * 获取用户id
+     *
+     * @return user_id - 用户id
+     */
+    public String getUserId() {
+        return userId;
+    }
+
+    /**
+     * 设置用户id
+     *
+     * @param userId 用户id
+     */
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
---
+++ src/main/java/com/clawhub/crm/mapper/DepartmentMapper.java
@@ -0,0 +1,7 @@
+package com.clawhub.crm.mapper;
+
+import com.clawhub.crm.common.IMapper;
+import com.clawhub.crm.entity.Department;
+
+public interface DepartmentMapper extends IMapper<Department> {
+}
